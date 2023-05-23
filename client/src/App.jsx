304abf25e02e861599f9036bd779292633865e66
Bogdan Ciornohac <bogdanciornohac@gmail.com>
2023-05-24 00:24:53 +0300

    build server side + some ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const [backendData, setBackendData] = useState([{}]);
   const [newText, setNewText] = useState('');
 
+
+
   useEffect(() => {
     fetch('http://localhost:5000/api')
       .then(response => response.json())
@@ -13,15 +15,45 @@ function App() {
   }, [])
 
 
+  const handleSubmit = () => {
+    const incercare = {
+      text: newText,
+      done: false
+    }
+
+    if (newText == '') {
+      alert('You can not add an empty note')
+    } else {
+      fetch('http://localhost:5000/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(incercare)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+          setBackendData(data)
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  };
   return (
     <div className="container">
       <div className="input-container">
-        <input className="input-td" onChange={event => { setNewText(event.target.value) }} placeholder="Add something to do here..." ></input>
-        {newText}
+        <input className="input-td"
+          value={newText}
+          onChange={event => { setNewText(event.target.value) }}
+          placeholder="Add something to do here..."
+        />
+        <button className="add-btn" onClick={handleSubmit}>Add ToDo</button>
       </div>
       <div className="card-container">
-        {typeof backendData.users === 'undefined' ? <p>Loading...</p> :
-          backendData.users.map((user, index) => <Card key={index} name={user} />)}
+        {typeof backendData.toDo === 'undefined' ? <p>Loading...</p> :
+          backendData.toDo.map((text, index) => <Card key={index} name={backendData.toDo[index].text} />)}
       </div>
       <div className='spacer layer1'></div>
     </div>
--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const cors = require("cors");
-
+const bodyParser = require('body-parser');
+const data = require("./data.json")
 
 
 const app = express()
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
-    res.json({ "users": ["ceva", "Lorem", "Ipsun", "alin","Dorm","make laba"] })
+    res.json(data)
+})
+
+app.post("/api",(req,res)=>{
+    const { text, done } = req.body;
+    data.toDo.push({ text,done }) 
+    console.log('Received data:', { text, done });
+    res.json(data);
 })
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
